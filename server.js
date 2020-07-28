154cd7c298bdcb86e4571cea4b5168997c11273f
taurtk <taurtk@gmail.com>
2020-07-28 11:34:42 +0545

    added get to server.js

--- server.js
+++ server.js
@@ -1,8 +1,12 @@
 const express= require('express');
 const mongoose= require('mongoose');
 const bodyParser= require('body-parser');
+
+const User = require('./models/User.js')
+mongoose.connect('mongodb://localhost/userData')
+
 const port=8000;
-const app= express();
+const app = express();
 
 app.use(bodyParser.json());
 
@@ -12,13 +16,47 @@ app.listen(port, ()=>{
 
 // CREATE
 app.post('/users',(req,res)=>{
-  // User.create()
+  User.create(
+    {
+      name:req.body.newData.name,
+      email:req.body.newData.email,
+      password:req.body.newData.password
+    },
+    (err,data)=>{
+    if (err){
+      res.json({success: false,message: err})
+    } else if (!data){
+      res.json({success: false,message: "Not Found"})
+    } else {
+      res.json({success: true,data: data})
+    }
+  })
 })
 
 app.route('/users/:id')
 // READ
 .get((req,res)=>{
-  // User.findById()
+   User.findById(req.params.id,(err,data)=>{
+     if(err)
+     {
+       res.json({
+         success:false,
+         message:err})
+     }
+     else if (!data){
+       res.json({
+         success:false,
+         message:"Not Found"
+       })
+     }
+     else{
+       res.json({
+         success:true,
+         data:data
+       })
+     }
+   })
+
 })
 // UPDATE
 .put((req,res)=>{
